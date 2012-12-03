faed6c3d93622c7b7c520fff45dea62b0cdac4ba
Hiroki Kondo <kompiro@gmail.com>
2012-12-03 16:52:51 +0900

    add real database connection test

--- src/test/java/com/change_vision/astah/extension/plugin/dbreverse/reverser/DBToProjectTest.java
+++ src/test/java/com/change_vision/astah/extension/plugin/dbreverse/reverser/DBToProjectTest.java
@@ -2,45 +2,65 @@ package com.change_vision.astah.extension.plugin.dbreverse.reverser;
 
 import java.io.File;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import com.change_vision.astah.extension.plugin.dbreverse.reverser.model.ConnectionInfo;
 import com.change_vision.astah.extension.plugin.dbreverse.reverser.model.ERRelationshipInfo;
 import com.change_vision.astah.extension.plugin.dbreverse.reverser.model.TableInfo;
-import com.change_vision.jude.api.inf.exception.InvalidEditingException;
-import com.change_vision.jude.api.inf.exception.LicenseNotFoundException;
-import com.change_vision.jude.api.inf.exception.ProjectLockedException;
 
 public class DBToProjectTest {
 	
 	@Rule
 	public TemporaryFolder folder = new TemporaryFolder();
+    private DBReader reader;
+    private List<TableInfo> tables;
+    private List<ERRelationshipInfo> relationships;
+	
+	@Before
+    public void before() throws Exception {
+        reader = new DBReader();
+        URL jarURL = DBToProjectTest.class.getResource("h2-1.3.170.jar");
+        String path = jarURL.getPath();
+        ConnectionInfo info = new ConnectionInfo();
+        info.setPathfile(path);
+        info.setClassname("org.h2.Driver");
+        info.setLogin("sa");
+        info.setPassword("");
+        URL dataURL = this.getClass().getResource("/data");
+        info.setJdbcurl("jdbc:h2:file:" + dataURL.getPath() + "/h2");
+        reader.connect(info);
+        tables = reader.getTables("H2", "PUBLIC");
+        relationships = reader.getRelationships("H2", "PUBLIC");
+    }
+	
+	@After
+    public void after() throws Exception {
+        reader.close();
+    }
 
 	@Test
-	public void testImportToProject_空ファイルを指定しても落ちないこと() throws LicenseNotFoundException, ProjectLockedException, InvalidEditingException, Throwable {
+	public void importToProjectWithSpecifyNotExistFile() throws Exception {
 		DBToProject dbToProject = new DBToProject();
 		File projectFile = folder.newFile("test.asta");
 		String projectFilePath = projectFile.getAbsolutePath();
-		List<TableInfo> tables = new ArrayList<TableInfo>();
-		List<ERRelationshipInfo> relationships = new ArrayList<ERRelationshipInfo>();
-
+		
 		dbToProject.importToProject(projectFilePath, tables, relationships);
 	}
 	
 	@Test
-	public void testImportToProject_空のastaファイルを指定しても落ちないこと() throws LicenseNotFoundException, ProjectLockedException, InvalidEditingException, Throwable {
-		DBToProject dbToProject = new DBToProject();
-		List<TableInfo> tables = new ArrayList<TableInfo>();
-		List<ERRelationshipInfo> relationships = new ArrayList<ERRelationshipInfo>();
-		
+	public void importProjectWithSpecifyEmptyFile() throws Exception {
+		DBToProject dbToProject = new DBToProject();		
 		URL resource = getClass().getResource("empty.asta");
 
 		String projectFilePath = resource.getFile();
 		dbToProject.importToProject(projectFilePath , tables, relationships);
 	}
+	
 
 }
