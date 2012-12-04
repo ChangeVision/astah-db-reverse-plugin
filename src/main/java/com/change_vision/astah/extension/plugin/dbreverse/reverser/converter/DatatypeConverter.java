951bebb069e018540c38909445040b99e6d4f9c4
Hiroki Kondo <kompiro@gmail.com>
2012-12-04 16:06:20 +0900

    extracted datatype converter

---
+++ src/main/java/com/change_vision/astah/extension/plugin/dbreverse/reverser/converter/DatatypeConverter.java
@@ -0,0 +1,26 @@
+package com.change_vision.astah.extension.plugin.dbreverse.reverser.converter;
+
+import com.change_vision.astah.extension.plugin.dbreverse.reverser.model.DatatypeInfo;
+import com.change_vision.jude.api.inf.editor.ERModelEditor;
+import com.change_vision.jude.api.inf.exception.InvalidEditingException;
+import com.change_vision.jude.api.inf.model.IERDatatype;
+import com.change_vision.jude.api.inf.model.IERModel;
+
+public class DatatypeConverter {
+
+    private ERModelEditor editor;
+    private IERModel erModel;
+
+    public DatatypeConverter(ERModelEditor editor, IERModel erModel) {
+        this.editor = editor;
+        this.erModel = erModel;
+    }
+
+    public IERDatatype convert(DatatypeInfo datatypeInfo) throws InvalidEditingException {
+        if(datatypeInfo == null) throw new IllegalArgumentException("datatype is null.");
+        String name = datatypeInfo.getName();
+        if (name == null || name.isEmpty()) throw new IllegalArgumentException("name is empty.");
+        return editor.createERDatatype(erModel, name);
+    }
+
+}
---
+++ src/test/java/com/change_vision/astah/extension/plugin/dbreverse/reverser/converter/DatatypeConverterTest.java
@@ -0,0 +1,60 @@
+package com.change_vision.astah.extension.plugin.dbreverse.reverser.converter;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.change_vision.astah.extension.plugin.dbreverse.reverser.model.DatatypeInfo;
+import com.change_vision.jude.api.inf.editor.ERModelEditor;
+import com.change_vision.jude.api.inf.model.IERDatatype;
+import com.change_vision.jude.api.inf.model.IERModel;
+
+public class DatatypeConverterTest {
+    
+    @Mock
+    private ERModelEditor editor;
+
+    @Mock
+    private IERModel erModel;
+
+    @Mock
+    private IERDatatype datatype;
+
+    private DatatypeConverter converter;
+    
+    @Before
+    public void before() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void convertWithNull() throws Exception {
+        converter = new DatatypeConverter(editor,erModel);
+        DatatypeInfo datatypeInfo = null;
+        converter.convert(datatypeInfo);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void convertWithEmptyName() throws Exception {
+        converter = new DatatypeConverter(editor,erModel);
+        DatatypeInfo datatypeInfo = new DatatypeInfo();
+        converter.convert(datatypeInfo);
+    }
+    
+    @Test
+    public void convert() throws Exception {
+        when(editor.createERDatatype(erModel, "hoge")).thenReturn(datatype);
+        converter = new DatatypeConverter(editor,erModel);
+        DatatypeInfo datatypeInfo = new DatatypeInfo();
+        datatypeInfo.setName("hoge");
+        IERDatatype converted = converter.convert(datatypeInfo);
+        assertThat(converted,is(notNullValue()));
+    }
+
+}
