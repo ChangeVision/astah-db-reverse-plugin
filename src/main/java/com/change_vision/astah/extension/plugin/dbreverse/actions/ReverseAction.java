7a82e15680fc58d837e2b6f6cd76b79a6013c772
Hiroki Kondo <kompiro@gmail.com>
2012-12-13 18:43:17 +0900

    merged with template file

--- src/main/java/com/change_vision/astah/extension/plugin/dbreverse/actions/ReverseAction.java
+++ src/main/java/com/change_vision/astah/extension/plugin/dbreverse/actions/ReverseAction.java
@@ -18,7 +18,7 @@ public class ReverseAction implements IPluginActionDelegate {
 	private AstahAPIWrapper apiUtil  = new AstahAPIWrapper();
 
 	public Object run(IWindow window) throws UnExpectedException {
-	    if (apiUtil.isEditing()) {
+//	    if (apiUtil.isEditing()) {
 			int result = JOptionPane.showConfirmDialog(
 			        window.getParent(), 
 			        Messages.getMessage("warning_message.save_before_reverse_warning"), 
@@ -50,7 +50,7 @@ public class ReverseAction implements IPluginActionDelegate {
 				default:
 					return null;
 			}
-		}
+//		}
 
 		JFrame frame = (JFrame) window.getParent();
 		ReverseDialog.getInstance(frame).showView();
